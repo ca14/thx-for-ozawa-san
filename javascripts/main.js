cb307e29c3ed79b7ad61abcedf2350eb20357e8d
jigsaw <jigsaw@live.jp>
2014-03-27 23:15:01 +0900

    update

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,6 @@
-var decoDisplay, logoDisplay;
+var decoDisplay, displayAll, loadHandler, loadedImages, logoDisplay;
+
+loadedImages = 0;
 
 logoDisplay = function() {
   return $(".logo-container").transition({
@@ -12,10 +14,21 @@ decoDisplay = function() {
   }, 500, "linear");
 };
 
-$(document).ready(function() {
+displayAll = function() {
   $(".ca14-container").transition({
     y: "-400px"
   }, 500, "linear");
   setTimeout(logoDisplay, 1000);
   return setTimeout(decoDisplay, 1200);
+};
+
+loadHandler = function() {
+  loadedImages++;
+  if (loadedImages === 2) {
+    return displayAll();
+  }
+};
+
+$(document).ready(function() {
+  return $("img").load(loadHandler);
 });
