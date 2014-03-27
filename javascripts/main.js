9dc3d7455e9d575b8cbfe6404ec1552d14607b29
jigsaw <jigsaw@live.jp>
2014-03-27 23:20:20 +0900

    update

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,19 +5,19 @@ loadedImages = 0;
 logoDisplay = function() {
   return $(".logo-container").transition({
     y: "200px"
-  }, 500, "linear");
+  }, 800, "linear");
 };
 
 decoDisplay = function() {
   return $(".deco-container").transition({
     opacity: 1
-  }, 500, "linear");
+  }, 800, "linear");
 };
 
 displayAll = function() {
   $(".ca14-container").transition({
     y: "-400px"
-  }, 500, "linear");
+  }, 800, "linear");
   setTimeout(logoDisplay, 1000);
   return setTimeout(decoDisplay, 1200);
 };
